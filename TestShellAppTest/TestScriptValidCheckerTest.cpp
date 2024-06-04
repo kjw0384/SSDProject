d1483c277a82f81af7623c5d79b1753a46433f9c
JuyoungKimmy-Kim <kimmykim.jy@gmail.com>
2024-06-04 15:41:08 +0900

    [feature/validChecker][RED] add isValidCommand TC

---
+++ TestShellAppTest/TestScriptValidCheckerTest.cpp
@@ -0,0 +1,38 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "../TestShellApp/TestScriptValidChecker.cpp"
+
+class CommandValidationTest : public ::testing::TestWithParam<std::string> {};
+
+INSTANTIATE_TEST_CASE_P(CommandStrings,
+    CommandValidationTest,
+    ::testing::Values(
+        "write 3 0xAAAABBBB", "read 3","exit",
+        "help","fullwrite", "fullread"
+    ));
+
+class InvalidCommandValidationTest : public ::testing::TestWithParam<std::string> {};
+
+INSTANTIATE_TEST_CASE_P(InvalidCommandStrings,
+    InvalidCommandValidationTest,
+    ::testing::Values(
+        "write 3", "read 3 0xAAAABBBB", "exit 3",
+        "helpp", "fullwrite 3", "fullread 0xAAAABBBB"
+    ));
+
+TEST_P(CommandValidationTest, IsValidCommand) {
+
+    TestScriptValidChecker checker;
+
+    const std::string& command = GetParam();
+    EXPECT_TRUE(checker.isValidCommand(command));
+}
+
+TEST_P(InvalidCommandValidationTest, IsNotValidCommand) {
+
+    TestScriptValidChecker checker;
+
+    const std::string& command = GetParam();
+    EXPECT_FALSE(checker.isValidCommand(command));
+}
