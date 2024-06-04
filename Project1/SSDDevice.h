17a88b5e097891ba5e17844b1c5e37b9d9bf606b
DESKTOP-HAGMG1Q\user <dongww12@gmail.com>
2024-06-04 15:43:29 +0900

    [feature/MockSSDDevice][GREEN] Add mock SSD device

--- Project1/SSDDevice.h
+++ Project1/SSDDevice.h
@@ -6,12 +6,12 @@ using namespace std;
 
 class SSDDevice {
 public:
-    void read(const int address) {
+    virtual void read(const int address) {
         cout << "Reading from LBA: " << address << endl;
         // 실제 읽기 작업 구현
     }
 
-    void write(const int address, const string& data) {
+    virtual void write(const int address, const string& data) {
         cout << "Writing data to LBA: " << address << endl;
         // 실제 쓰기 작업 구현
     }
--- VirtualSSDTest/test.cpp
+++ VirtualSSDTest/test.cpp
@@ -1,7 +1,35 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include "../Project1/ReadCommand.cpp"
+#include "../Project1/WriteCommand.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+using namespace testing;
+
+class MockSSDDevice : public SSDDevice {
+public:
+	MOCK_METHOD(void, read, (const int address), (override));
+	MOCK_METHOD(void, write, (const int address, const string& data), (override));
+};
+
+class CommandTestFixture : public testing::Test {
+public:
+	MockSSDDevice mockDevice;
+	const int address = 7;
+	const string data = "0x12345678";
+};
+
+TEST_F(CommandTestFixture, Read) {
+	EXPECT_CALL(mockDevice, read)
+		.Times(1);
+
+	ReadCommand readCmd(&mockDevice, address);
+	readCmd.execute();
+}
+
+TEST_F(CommandTestFixture, Write) {
+	EXPECT_CALL(mockDevice, write)
+		.Times(1);
+
+	WriteCommand writeCmd(&mockDevice, address, data);
+	writeCmd.execute();
 }
