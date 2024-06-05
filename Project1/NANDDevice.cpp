07f1ce06f30d4af4fe9b4cc959e913fbc769c007
DESKTOP-5FBE16T\User <blue1336@naver.com>
2024-06-05 15:46:14 +0900

    [feature/FileReadWrite][GREEN] separate NANDDevice class to h and cpp file

---
+++ Project1/NANDDevice.cpp
@@ -0,0 +1,20 @@
+#include "NANDDevice.h"
+
+void NANDDevice::read(const int address) {
+    FileManager& fileManager = FileManager::getInstance();
+    fileManager.setFilePath();
+
+    vector<string> getData = fileManager.readFromNand();
+    fileManager.writeToResult(getData[address]);
+    cout << "Read from LBA: " << address << ", value: " << getData[address] << endl;
+}
+
+void NANDDevice::write(const int address, const string& data) {
+    FileManager& fileManager = FileManager::getInstance();
+    fileManager.setFilePath();
+
+    vector<string> setData = fileManager.readFromNand();
+    setData[address] = data;
+    fileManager.writeToNand(setData);
+    cout << "Write to LBA: " << address << ", value: " << setData[address] << endl;
+}
--- Project1/NANDDevice.h
+++ Project1/NANDDevice.h
@@ -1,30 +1,13 @@
 #pragma once
-
 #include <iostream>
-#include <string>
 #include <vector>
+#include <string>
 #include "FileManager.h"
 
 using namespace std;
 
 class NANDDevice {
 public:
-    virtual void read(const int address) {
-        FileManager& fileManager = FileManager::getInstance();
-        fileManager.setFilePath();
-
-        vector<string> getData = fileManager.readFromNand();
-        fileManager.writeToResult(getData[address]);
-        cout << "Read from LBA: " << address << ", value: " << getData[address]<<endl;
-    }
-
-    virtual void write(const int address, const string& data) {
-        FileManager& fileManager = FileManager::getInstance();
-        fileManager.setFilePath();
-
-        vector<string> setData = fileManager.readFromNand();
-        setData[address] = data;
-        fileManager.writeToNand(setData);
-        cout << "Write to LBA: " << address << ", value: " << setData[address] << endl;
-    }
+    virtual void read(const int address);
+    virtual void write(const int address, const string& data);
 };
