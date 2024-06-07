97e0b5512a25f692361860a9f599c3c6efd678ba
DESKTOP-5FBE16T\User <blue1336@naver.com>
2024-06-07 17:07:32 +0900

    [GREEN]  testscript implementation

--- TestScenario/TestScenario.cpp
+++ TestScenario/TestScenario.cpp
@@ -1,19 +1,55 @@
 #include "TestScenario.h"
 #include "../TestShellApp/IScenario.h"
+#include <sstream>
 
 class test1app : public IScenario {
 public:
-	void run() override {
-		cout << "runScenario()\n";
-	}
+	test1app();
+	void run() override;
+private:
 };
+test1app::test1app() {
+	vector<string> scenarios{
+		"read 1 0x11111111",
+		"write 7 0x22222222",
+		"read 2"
+	};
+
+	for (const auto& scenario : scenarios) {
+		Command cmd = parseCommand(scenario);
+		m_TestScriptCommandVector.push_back(cmd);
+	}
+}
+void test1app::run() {
+	// 여기에 실행 로직을 추가하세요
+	for (const auto& command : m_TestScriptCommandVector) {
+		cout << "Type: " << command.type << ", LBA Index: " << command.LBAIndexNum << ", Value: " << command.value << endl;
+	}
+}
 
 class test2app : public IScenario {
 public:
-	void run() override {
-		cout << "runScenario()\n";
-	}
+	test2app();
+	void run() override;
+private:
 };
+test2app::test2app() {
+	vector<string> scenarios{
+		"read 3 0x33333333",
+		"write 5 0x55555555",
+		"read 5"
+	};
+
+	for (const auto& scenario : scenarios) {
+		Command cmd = parseCommand(scenario);
+		m_TestScriptCommandVector.push_back(cmd);
+	}
+}
+void test2app::run() {
+	for (const auto& command : m_TestScriptCommandVector) {
+		cout << "Type: " << command.type << ", LBA Index: " << command.LBAIndexNum << ", Value: " << command.value << endl;
+	}
+}
 
 IScenario* TestScenario::getScenario(const string& testScenario)
 {
--- TestShellApp/IScenario.h
+++ TestShellApp/IScenario.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <iostream>
+#include <sstream>
+#include "TestCommand.h"
 
 #define interface struct
 
@@ -7,6 +9,33 @@ using std::cout;
 
 interface IScenario {
 public:
+	typedef vector<Command> TestVector_t;
 	virtual ~IScenario() {}
 	virtual void run() = 0;
+	Command parseCommand(const string& scenario) {
+		Command cmd;
+		istringstream iss(scenario);
+		string value;
+
+		iss >> cmd.type >> cmd.LBAIndexNum;
+
+		if (iss >> value) {
+			cmd.value = value;
+		}
+		else {
+			cmd.value = "";
+		}
+
+		return cmd;
+	}
+
+	void addCommand(const Command& cmd) {
+		m_TestScriptCommandVector.push_back(cmd);
+	}
+
+	const TestVector_t& getCommands() const {
+		return m_TestScriptCommandVector;
+	}
+
+	TestVector_t m_TestScriptCommandVector;
 };
