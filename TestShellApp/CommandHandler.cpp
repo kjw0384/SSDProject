413ea20cd8ee5ff816e20d46fa372c6a0eb3beaf
DESKTOP-5FBE16T\User <blue1336@naver.com>
2024-06-10 14:51:03 +0900

    Merge branch 'master' into feature/TestScriptDataBase_Imple
    # Conflicts:
    #	TestScenario/TestScriptDescriptor.h
    #	TestShellApp/main.cpp

--- TestShellApp/CommandHandler.cpp
+++ TestShellApp/CommandHandler.cpp
@@ -13,6 +13,7 @@ Result_e CommandHandler::runCommand(const string& testScript) {
         if (checker.isValidScenario(testScript, testScenario) == false) {
             return Result_e::FAIL;
         }
+        command.type = "testcase";
         handleScenario(testScript, testScenario);
         return Result_e::SUCCESS;
     }
