8b431e4601c14a71d39c919def0a415a8a1dcdbe
DESKTOP-5FBE16T\User <kjw0384@gmail.com>
2024-06-10 09:09:41 +0900

    [Logger][Refactor] Regex 반영  - EXPECT_THAT의 MatchesRegex는 제한된 구문만 사용 가능하여 regex library 사용함.

--- TestShellAppTest/LoggerTest.cpp
+++ TestShellAppTest/LoggerTest.cpp
@@ -2,6 +2,7 @@
 #include "gmock/gmock.h"
 #include "../TestShellApp/Logger.cpp"
 #include <filesystem>
+#include <regex>
 using namespace testing;
 
 
@@ -9,12 +10,11 @@ TEST(LoggerTest, writeLog) {
 	if (filesystem::exists(LOG_DIR))
 		filesystem::remove_all(LOG_DIR);
 	Logger::writeLog("abcd", __FUNCTION__);
+	
 	ifstream file(LOG_FULL_PATH_NAME);
 	string data;
 	getline(file, data);
-	EXPECT_THAT(data, MatchesRegex("\\[.*\\] .* : abcd$"));
-	// TODO : regex change
-	//EXPECT_THAT(data, MatchesRegex("\\[[0-9]{2}\\.[0-9]{2}\\.[0-9]{2} [0-9]{2}h:[0-9]{2}m\\] .{50} : abcd$"));
-	//EXPECT_THAT(data, MatchesRegex("\[[0-9]{2}\.[0-9]{2}\.[0-9]{2} [0-9]{2}h:[0-9]{2}m\] .{50} : abcd$"));
+	
+	regex txt_regex("\\[[0-9]{2}\\.[0-9]{2}\\.[0-9]{2} [0-9]{2}h:[0-9]{2}m\\] .{50} : abcd$");
+	EXPECT_EQ(regex_match(data, txt_regex), true);
 }
-
