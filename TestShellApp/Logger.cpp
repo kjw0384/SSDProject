9e75d9a4cf722a8996bbcef560965043e1873af7
DESKTOP-5FBE16T\User <kjw0384@gmail.com>
2024-06-10 09:32:38 +0900

    [Logger][RedGreen] 10K넘을 시 File Backup case

--- TestShellApp/Logger.cpp
+++ TestShellApp/Logger.cpp
@@ -7,6 +7,7 @@
 #include <sys/stat.h>
 #include <filesystem>
 #include <direct.h>
+#include <regex>
 
 #define LOG_MESSAGE(message) Logger::log(message, __FUNCTION__)
 
@@ -16,9 +17,6 @@ void Logger::log(string inputStr, const char* function) {
 }
 
 void Logger::writeLog(string inputStr, const char* function) {
-
-	cout << "s:" << inputStr << endl;
-	cout << "function:" << function << endl;
 
 	if (filesystem::exists(LOG_DIR) == false)
 	{
@@ -40,11 +38,47 @@ void Logger::writeLog(string inputStr, const char* function) {
 	localtime_s(&tmNow, &now);
 	char logstr[100];
 	strftime(logstr, sizeof(logstr), "[%y.%m.%d %Hh:%Mm]", &tmNow);
-	logfile << format("{0} {1:50s} : {2}", (string)logstr, function, inputStr);
+	logfile << format("{0} {1:50s} : {2}\n", (string)logstr, function, inputStr);
 	logfile.close();
 }
 
 void Logger::readyLogFile(string fileName) {
 
+	int fileSize = filesystem::file_size(fileName);
+	vector<string> filelist;
+
+	if (fileSize < 10 * 1024)
+		return;
+
+	//rename before
+	if (filesystem::exists(LOG_DIR)) {
+		for (const auto& entry : filesystem::directory_iterator(LOG_DIR)) {
+			if (entry.is_regular_file()) {
+				filelist.push_back(entry.path().filename().string());
+			}
+		}
+	}
+
+	for (int i = 0; i < filelist.size(); i++) {
+		regex txt_regex("^until_[0-9]{6}_[0-9]{2}h_[0-9]{2}m_[0-9]{2}s.log$");
+
+		if (filelist[i] == LOG_FILE_NAME)
+			continue;
+		else if (regex_match(filelist[i], txt_regex))
+		{
+			//rename to zip
+			string newfile = filelist[i].substr(0, filelist[i].length() - 4) + ".zip";
+			filesystem::rename(LOG_DIR + "/" + filelist[i], LOG_DIR + "/" + newfile);
+		}
+	}
+
+	//filesystem::rename
+	//rename current
+	time_t now = time(nullptr);
+	tm tmNow;
+	localtime_s(&tmNow, &now);
+	char untilLogFileName[100];
+	strftime(untilLogFileName, sizeof(untilLogFileName), "until_%y%m%d_%Hh_%Mm_%Ss.log", &tmNow);
+	filesystem::rename(fileName, LOG_DIR + "/" + (string)untilLogFileName);
 	return;
 }
--- TestShellAppTest/LoggerTest.cpp
+++ TestShellAppTest/LoggerTest.cpp
@@ -5,6 +5,20 @@
 #include <regex>
 using namespace testing;
 
+int getRegexMatchingFileCount(string fileFormat) {
+	int nResult = 0;
+	if (filesystem::exists(LOG_DIR)) {
+		for (const auto& entry : filesystem::directory_iterator(LOG_DIR)) {
+			if (entry.is_regular_file()) {
+				string fileName = entry.path().filename().string();
+				regex txt_regex(fileFormat);
+				if (regex_match(fileName, txt_regex) == true)
+					nResult++;
+			}
+		}
+	}
+	return nResult;
+}
 
 TEST(LoggerTest, writeLog) {
 	if (filesystem::exists(LOG_DIR))
@@ -17,4 +31,22 @@ TEST(LoggerTest, writeLog) {
 	
 	regex txt_regex("\\[[0-9]{2}\\.[0-9]{2}\\.[0-9]{2} [0-9]{2}h:[0-9]{2}m\\] .{50} : abcd$");
 	EXPECT_EQ(regex_match(data, txt_regex), true);
+}
+
+TEST(LoggerTest, checkBackup) {
+	if (filesystem::exists(LOG_DIR))
+		filesystem::remove_all(LOG_DIR);
+
+	for (int i = 0; i < 150; i++)
+		Logger::writeLog("abcd", __FUNCTION__);
+
+	LOG_MESSAGE("efgh");
+
+	for (int i = 0; i < 150; i++)
+		Logger::writeLog("qwer", __FUNCTION__);
+
+	LOG_MESSAGE("fdsa");
+	EXPECT_EQ(getRegexMatchingFileCount("^until_[0-9]{6}_[0-9]{2}h_[0-9]{2}m_[0-9]{2}s.log$"), 1);
+	EXPECT_EQ(getRegexMatchingFileCount("^until_[0-9]{6}_[0-9]{2}h_[0-9]{2}m_[0-9]{2}s.zip$"), 1);
+	EXPECT_EQ(getRegexMatchingFileCount("^latest.log$"), 1);
 }
