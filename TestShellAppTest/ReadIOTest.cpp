8b7a0f3eabb25ac9a604e727a3b51be07c1daf86
Dongwoo Kim <dongww12@gmail.com>
2024-06-11 10:55:33 +0900

    [feature/TestShellTestFormat] Apply clang-format to TestShellAppTest

--- TestShellAppTest/ReadIOTest.cpp
+++ TestShellAppTest/ReadIOTest.cpp
@@ -1,36 +1,38 @@
-#include "gtest/gtest.h"
-#include "gmock/gmock.h"
 #include "../TestShellApp/ReadIOInterface.h"
 #include "../TestShellApp/VirtualSsdProcess.cpp"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
 
 using namespace testing;
 
-class MockReadIO : public ReadIOInterface {
-public:
+class MockReadIO : public ReadIOInterface
+{
+  public:
     MOCK_METHOD(string, GetReadResult, (), (override));
 };
 
-TEST(TestShellTestSuite, ReadIO) {
+TEST(TestShellTestSuite, ReadIO)
+{
     MockReadIO mockReadIO;
 
-    EXPECT_CALL(mockReadIO, GetReadResult)
-        .Times(AtLeast(1))
-        .WillRepeatedly(Return("0x12345678"));
+    EXPECT_CALL(mockReadIO, GetReadResult).Times(AtLeast(1)).WillRepeatedly(Return("0x12345678"));
 
     EXPECT_EQ(mockReadIO.GetReadResult(), "0x12345678");
 }
 
-TEST(VirtaulSsdProcessTestSuite, Write) {
+TEST(VirtaulSsdProcessTestSuite, Write)
+{
     VirtaulSsdProcess virProc;
-    const int addr{ 7 };
-    const string data{ "0x99998888" };
+    const int addr{7};
+    const string data{"0x99998888"};
 
     EXPECT_EQ(virProc.sendWriteIpc(addr, data), Result_e::SUCCESS);
 }
 
-TEST(VirtaulSsdProcessTestSuite, Read) {
+TEST(VirtaulSsdProcessTestSuite, Read)
+{
     VirtaulSsdProcess virProc;
-    const int addr{ 7 };
+    const int addr{7};
 
     EXPECT_EQ(virProc.sendReadIpc(addr), Result_e::SUCCESS);
 }
--- TestShellAppTest/TestScriptValidCheckerTest.cpp
+++ TestShellAppTest/TestScriptValidCheckerTest.cpp
@@ -1,39 +1,34 @@
-#include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include "gtest/gtest.h"
 
 #include "../TestShellApp/TestScriptValidChecker.cpp"
 
-class CommandValidationVectorStringTest : public ::testing::TestWithParam<std::vector<std::string>> {};
+class CommandValidationVectorStringTest : public ::testing::TestWithParam<std::vector<std::string>>
+{
+};
 
-INSTANTIATE_TEST_SUITE_P(CommandVectorStrings,
-    CommandValidationVectorStringTest,
-    ::testing::Values(
-        vector<string>{ "write", "3", "0xAAAABBBB" },
-        vector<string>{ "read", "3"},
-        vector<string>{ "exit" },
-        vector<string>{ "help" },
-        vector<string>{ "fullwrite", "0x11112222" },
-        vector<string>{ "fullread" }
-    ));
+INSTANTIATE_TEST_SUITE_P(CommandVectorStrings, CommandValidationVectorStringTest,
+                         ::testing::Values(vector<string>{"write", "3", "0xAAAABBBB"}, vector<string>{"read", "3"},
+                                           vector<string>{"exit"}, vector<string>{"help"},
+                                           vector<string>{"fullwrite", "0x11112222"}, vector<string>{"fullread"}));
 
-class CommandInvalidationVectorStringTest : public ::testing::TestWithParam<std::vector<std::string>> {};
+class CommandInvalidationVectorStringTest : public ::testing::TestWithParam<std::vector<std::string>>
+{
+};
 
-INSTANTIATE_TEST_SUITE_P(InvalidCommandVectorStrings,
-    CommandInvalidationVectorStringTest,
-    ::testing::Values(
-        vector<string>{ "write", "3"},
-        vector<string>{ "read", "3", "0xAAAABBBB" },
-        vector<string>{ "exit", "3"},
-        vector<string>{ "helpp"},
-        vector<string>{ "helpp", "3"},
-        vector<string>{ "fullread", "0xAAAABBBB"}
-    ));
+INSTANTIATE_TEST_SUITE_P(InvalidCommandVectorStrings, CommandInvalidationVectorStringTest,
+                         ::testing::Values(vector<string>{"write", "3"}, vector<string>{"read", "3", "0xAAAABBBB"},
+                                           vector<string>{"exit", "3"}, vector<string>{"helpp"},
+                                           vector<string>{"helpp", "3"}, vector<string>{"fullread", "0xAAAABBBB"}));
 
-string vectorStringToString(const vector<string>& testScriptTokens) {
+string vectorStringToString(const vector<string> &testScriptTokens)
+{
 
     std::ostringstream oss{};
-    for (size_t i = 0; i < testScriptTokens.size(); ++i) {
-        if (i != 0) {
+    for (size_t i = 0; i < testScriptTokens.size(); ++i)
+    {
+        if (i != 0)
+        {
             oss << " ";
         }
         oss << testScriptTokens[i];
@@ -41,20 +36,22 @@ string vectorStringToString(const vector<string>& testScriptTokens) {
     return oss.str();
 }
 
-TEST_P(CommandValidationVectorStringTest, IsValidCommand) {
+TEST_P(CommandValidationVectorStringTest, IsValidCommand)
+{
 
     TestScriptValidChecker checker;
 
-    const vector<string>& command = GetParam();
+    const vector<string> &command = GetParam();
     string commandString = vectorStringToString(command);
     EXPECT_TRUE(checker.isValidCommand(commandString, command));
 }
 
-TEST_P(CommandInvalidationVectorStringTest, IsNotValidCommand) {
+TEST_P(CommandInvalidationVectorStringTest, IsNotValidCommand)
+{
 
     TestScriptValidChecker checker;
 
-    const vector<string>& command = GetParam();
+    const vector<string> &command = GetParam();
     string commandString = vectorStringToString(command);
     EXPECT_FALSE(checker.isValidCommand(commandString, command));
 }
